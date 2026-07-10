719418f5d8f594f709ebf0ca419ea11191ac89d5
Divianshu <divianshuchandel@gmail.com>
2026-07-10 23:11:16 +0530

    Create student_info.py

---
+++ Day-02/student_info.py
@@ -0,0 +1,26 @@
+# Day 2 - Variables & Data Types
+
+print("===== Student Information =====")
+
+# Variables
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+college = input("Enter your college: ")
+course = input("Enter your course: ")
+cgpa = float(input("Enter your CGPA: "))
+
+# Display information
+print("\n----- Student Details -----")
+print("Name:", name)
+print("Age:", age)
+print("College:", college)
+print("Course:", course)
+print("CGPA:", cgpa)
+
+# Data types
+print("\n----- Data Types -----")
+print("Name:", type(name))
+print("Age:", type(age))
+print("College:", type(college))
+print("Course:", type(course))
+print("CGPA:", type(cgpa))
